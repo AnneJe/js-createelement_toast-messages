0cbb986d1d3aadec10bc83e3edac807b832cf803
Anne Jerusalem <aliese.jeru@web.de>
2023-10-05 12:15:55 +0200

    Kommentarergängung

--- js/index.js
+++ js/index.js
@@ -6,8 +6,17 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+
+  //Tipp:
+  // append ist der Tipp für die Liste, man soll der Liste ein Element anfügen
+  //daher Listenelement Create und der Liste anfügen
+  const toast = document.createElement("li");
+  toast.classList.add("toast-container__message");
+  toast.textContent = "Im a toast message";
+  toastContainer.append(toast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
